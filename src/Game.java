357fc79312d813a00f091df019ef93fa81d8c13b
Laura Bassani <bassanilaurarita@gmail.com>
2021-04-06 19:16:20 +0200

    Strike case implementation

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,24 @@
 public class Game {
 
-    int points = 0;
+    int[] rolls = new int[20];
+    int i = 0;
 
-    int roll(int n ) {
-       this.points = this.points + n;
-        return this.points;
+    int roll(int n) {
+        rolls[i] = n;
+        i++;
+        return score();
+    }
+
+    private int score() {
+        int points = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            if (rolls[i] == 10){
+                points += rolls[i + 1];
+                points += rolls[i + 2];
+
+            }
+            points += rolls[i];
+        }
+        return points;
     }
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
@@ -5,14 +6,22 @@ class GameTest {
    @Test
     void shouldReturnPointsInRoll() {
        Game g = new Game();
-       assert (g.roll(4) == 4);
+       assert(g.roll(4) == 4);
    }
 
    @Test
    void shouldSumRollsPoints() {
        Game g = new Game();
-       assert (g.roll(6) == 6);
-       assert (g.roll(3) == 9);
+       assert(g.roll(6) == 6);
+       assert(g.roll(3) == 9);
+   }
+
+   @Test
+    void shouldSumNextFramePointsIfStrike() {
+       Game g = new Game();
+       g.roll(10);
+       g.roll(5);
+       Assertions.assertEquals(24 ,g.roll(2));
    }
 
 }
